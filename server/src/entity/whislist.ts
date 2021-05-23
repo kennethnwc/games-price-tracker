d17bc73cc3c489ac3aed31f31dd98b8446883c53
Ng Wing Chung <nwc27101994@yahoo.com.hk>
2021-05-23 14:33:45 +0800

    add wishlist model and user model

---
+++ server/src/entity/whislist.ts
@@ -0,0 +1,23 @@
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Game } from "./game";
+import { User } from "./user";
+
+@Entity()
+export class WishList extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Game, { nullable: false })
+  @JoinColumn({ name: "game_id" })
+  game: Game;
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+}
